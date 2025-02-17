3469114385fc69d7cac0be22fa6e402adeaf14a6
xingyuan wang <kinyou_xy@foxmail.com>
2025-02-17 09:48:53 +0800

    fix:修复回调消息json绑定后数字四舍五入的问题

--- api/khan/v1/callback.go
+++ api/khan/v1/callback.go
@@ -1,9 +1,11 @@
 package v1
 
+import "encoding/json"
+
 type CollectRequest struct {
-	AppId    string `json:"appid" validate:"required"`
-	TypeName string `json:"type_name" validate:"required"`
-	Data     any    `json:"data" validate:"required"`
+	AppId    string          `json:"appid" validate:"required"`
+	TypeName string          `json:"type_name" validate:"required"`
+	Data     json.RawMessage `json:"data" validate:"required"`
 }
 
 type SetCallbackRequest struct {
