7cbc7d3fdefc1e48fb58c57d7c1d839d8d000d57
xingyuan wang <kinyou_xy@foxmail.com>
2025-08-08 11:41:04 +0800

    fix:修复收藏非朋友圈导致的转发失败

--- internal/task/forward/chain/sns_detail_handler.go
+++ internal/task/forward/chain/sns_detail_handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	v1 "maogou/khan/api/khan/v1"
+	"maogou/khan/internal/constant"
 	"maogou/khan/internal/pkg/log"
 	"maogou/khan/internal/sdk/khan"
 )
@@ -26,6 +27,9 @@ func (sd *SnsDetailHandler) Handle(ctx context.Context, pld *PipelineData) error
 
 	if len(pld.SnsID) == 0 {
 		log.C(ctx).Warn().Str("appid", pld.AppID).Msg("snsID为空")
+		if err := sd.sdk.Rdb().Incr(ctx, constant.CurrentFavId+pld.AppID).Err(); err != nil {
+			log.C(ctx).Error().Err(err).Msg("调用rdb.Incr方法,更新下一次转发的收藏favId失败,请手动更新redis中的值")
+		}
 		return errors.New("snsID为空")
 	}
 
